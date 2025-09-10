415878cd05dc82712723c226292b54cd762c2ae1
Sebastian Safari <ssebs@ymail.com>
2025-09-10 00:03:17 -0700

    sorta working

--- scenes/motorcycle.gd
+++ scenes/motorcycle.gd
@@ -1,8 +1,9 @@
 class_name Motorcycle extends AnimatableBody3D
 
+@onready var anim_player: AnimationPlayer = %AnimationPlayer
 @onready var rotate_point: Node3D = %RotatePoint
 
-var gravity = 0.2
+var gravity = .98
 var disable_input = false
 
 func _ready():
@@ -10,19 +11,19 @@ func _ready():
 
 func handle_user_input(ret: Dictionary) -> Dictionary:
     if Input.is_action_pressed("throttle"):
-        ret['x_angle'] = 1
+        ret['x_angle'] = 2
     elif Input.is_action_pressed("brake"):
-        ret['x_angle'] = -1
+        ret['x_angle'] = -2
     
     if Input.is_action_pressed("lean_left"):
-        ret['z_angle'] = -1
-    elif Input.is_action_pressed("lean_right"):
         ret['z_angle'] = 1
+    elif Input.is_action_pressed("lean_right"):
+        ret['z_angle'] = -1
     
-    # if Input.is_action_pressed("lean_back"):
-    #     print("back")
-    # elif Input.is_action_pressed("lean_forward"):
-    #     print("forward")
+    if Input.is_action_pressed("lean_back"):
+        ret['x_angle'] += 1
+    elif Input.is_action_pressed("lean_forward"):
+        ret['x_angle'] -= 1
     
     return ret
 
@@ -34,19 +35,23 @@ func _physics_process(delta):
     if !disable_input:
         rotate_info = handle_user_input(rotate_info)
 
-    var x_angle = rotate_info['x_angle']
+    var x_angle_rad = rotate_info['x_angle']
 
-    rotate_point.rotate_x(x_angle * delta)
-    rotate_point.rotate_z(rotate_info["z_angle"] * delta)
+    var current_x_angle_deg = rotate_point.global_rotation_degrees.x
+    # print('x: ', current_x_angle_deg)
+    
+    if current_x_angle_deg > 0 && current_x_angle_deg <= 90:
+        x_angle_rad -= gravity
 
     
-    var actual_x_angle_rad = rotate_point.global_rotation_degrees.x
-    print('x: ', actual_x_angle_rad)
+    if current_x_angle_deg < 0:
+        x_angle_rad = 0
+        rotate_point.global_rotation_degrees.x = 0
+    elif current_x_angle_deg > 90:
+        disable_input = true
+        anim_player.play("crash")
 
-    if actual_x_angle_rad < 0:
-        actual_x_angle_rad = 0
-    elif actual_x_angle_rad > 0 && actual_x_angle_rad <= 90:
-        x_angle -= gravity
-    elif actual_x_angle_rad > 90:
-        # disable_input = true
-        print("flip, floop, ya screwed")
+    rotate_point.rotate_x(x_angle_rad * delta)
+    rotate_point.rotate_z(rotate_info["z_angle"] * delta)
+
+    # print('final x: ', rotate_point.global_rotation_degrees.x)
