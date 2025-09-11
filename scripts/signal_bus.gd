20964fb03ce331e193583036db4108c673c1bdef
Sebastian Safari <ssebs@ymail.com>
2025-09-10 19:42:11 -0700

    add bar ui

--- scripts/signal_bus.gd
+++ scripts/signal_bus.gd
@@ -7,3 +7,7 @@ var angle_deg: float:
 signal angle_updated(angle_deg: float)
 
 signal notify_ui(msg: String) # see ui.gd
+
+
+func _hide_warnings_in_editor():
+    notify_ui.emit('this should never show up')
