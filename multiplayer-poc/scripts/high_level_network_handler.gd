e37d9c40a86d9e1e1ee0fa646031602c1da6683c
Sebastian Safari <ssebs@ymail.com>
2025-12-13 11:49:26 -0800

    some multiplayer

--- multiplayer-poc/scripts/high_level_network_handler.gd
+++ multiplayer-poc/scripts/high_level_network_handler.gd
@@ -1,4 +1,5 @@
 extends Node
+signal host_started()
 
 const IP_ADDR: String = "localhost"
 const PORT: int = 42069
@@ -16,3 +17,11 @@ func start_client():
     peer = ENetMultiplayerPeer.new()
     peer.create_client(IP_ADDR, PORT)
     multiplayer.multiplayer_peer = peer
+
+# To create a server+player, or host player, we create a new function "start_host()".
+# This function then starts the server like normal, but also calls a signal "host_started".
+# Since this is an autoloaded class, other functions can connect to this signal, like I did
+# in the "high_level_player_spawner". More comments there.
+func start_host() -> void:
+    start_server()
+    host_started.emit()
--- multiplayer-poc/scripts/high_level_ui.gd
+++ multiplayer-poc/scripts/high_level_ui.gd
@@ -2,10 +2,12 @@ extends Control
 
 @onready var server_btn: Button = %Server
 @onready var client_btn: Button = %Client
+@onready var host_btn: Button = %Host
 
 func _ready():
     server_btn.pressed.connect(_on_server_pressed)
     client_btn.pressed.connect(_on_client_pressed)
+    host_btn.pressed.connect(_on_host_pressed)
 
 
 func _on_server_pressed():
@@ -13,3 +15,6 @@ func _on_server_pressed():
 
 func _on_client_pressed():
     HighLevelNetworkHandler.start_client()
+
+func _on_host_pressed():
+    HighLevelNetworkHandler.start_host()
