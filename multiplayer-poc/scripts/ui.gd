bcb17000668042e8ac659c165d5fb37d869d0986
Sebastian Safari <ssebs@ymail.com>
2025-12-13 14:58:59 -0800

    trying another tut... not working

--- multiplayer-poc/scripts/ui.gd
+++ multiplayer-poc/scripts/ui.gd
@@ -2,19 +2,16 @@ extends Control
 
 @onready var server_btn: Button = %Server
 @onready var client_btn: Button = %Client
-@onready var host_btn: Button = %Host
 
 func _ready():
     server_btn.pressed.connect(_on_server_pressed)
     client_btn.pressed.connect(_on_client_pressed)
-    host_btn.pressed.connect(_on_host_pressed)
 
 
 func _on_server_pressed():
-    HighLevelNetworkHandler.start_server()
+    self.hide()
+    MultiplayerManager.start_server()
 
 func _on_client_pressed():
-    HighLevelNetworkHandler.start_client()
-
-func _on_host_pressed():
-    HighLevelNetworkHandler.start_host()
+    self.hide()
+    MultiplayerManager.start_client()
