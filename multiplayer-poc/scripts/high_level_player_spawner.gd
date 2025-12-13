aaf7fc5e425b02bed3472427155e03cb2d8580e5
Sebastian Safari <ssebs@ymail.com>
2025-12-13 11:49:26 -0800

    some multiplayer

--- multiplayer-poc/scripts/high_level_player_spawner.gd
+++ multiplayer-poc/scripts/high_level_player_spawner.gd
@@ -4,6 +4,8 @@ extends MultiplayerSpawner
 
 func _ready():
     multiplayer.peer_connected.connect(spawn_player)
+    # Here we connect to the "host_started" signal in the high_level_network_handler class.
+    HighLevelNetworkHandler.host_started.connect(spawn_host_player)
 
 func spawn_player(id: int):
     if !multiplayer.is_server(): return
@@ -12,3 +14,10 @@ func spawn_player(id: int):
     player.name = str(id)
 
     get_node(spawn_path).call_deferred("add_child", player)
+
+
+# In this function, which is connected to the "host_started" signal in the high_level_network_handler
+# class, we spawn the server player. Easy right?
+func spawn_host_player() -> void:
+    if !multiplayer.is_server(): return
+    spawn_player(multiplayer.get_unique_id())
