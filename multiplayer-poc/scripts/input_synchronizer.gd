238d7ffad5e36986c10c89ce1e998be13e15325b
Sebastian Safari <ssebs@ymail.com>
2025-12-13 23:01:13 -0800

    working mp bullet

--- multiplayer-poc/scripts/input_synchronizer.gd
+++ multiplayer-poc/scripts/input_synchronizer.gd
@@ -1,5 +1,6 @@
-extends MultiplayerSynchronizer
+class_name InputSynchronizer extends MultiplayerSynchronizer
 
+@export var player: NetworkedPlayer
 @export var input_dir: Vector2
 
 func _ready():
@@ -10,3 +11,12 @@ func _ready():
 
 func _physics_process(_delta):
     input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+    if Input.is_action_just_pressed("ui_accept"):
+        shoot_bullet.rpc()
+
+@rpc("call_local")
+func shoot_bullet():
+    # Set the variable only on the server
+    if multiplayer.is_server():
+        player.shoot_bullet = true
