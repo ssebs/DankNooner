e634b6a6ca3f10b280db84594f6fbef9b5c27dde
Sebastian Safari <ssebs@ymail.com>
2025-12-13 17:01:39 -0800

    finally working mp

--- multiplayer-poc/TODO.md
+++ multiplayer-poc/TODO.md
@@ -1,12 +1,11 @@
 # TODO
 
 ## Up Next
-- Try low level https://www.youtube.com/watch?v=8GfJw0E5MFE&list=LL
 - Create lobby / connection system
 - Add RPC calls (e.g. damage)
 
 ## In Progress
-- Follow [tutorial](https://www.youtube.com/watch?v=YnfsyZJRsL8&list=FLcWCRWXSUwTMxV3GyuoTtDA&index=2)
+- Follow [tutorial](https://www.youtube.com/watch?v=V4a_J38XdHk&list=PLOtt3_R1rR9X3bm9lN0WtmI3tzm8utIHJ)
 - Sync 2 players in game
 
 ## Done
--- multiplayer-poc/scripts/input_synchronizer.gd
+++ multiplayer-poc/scripts/input_synchronizer.gd
@@ -7,20 +7,6 @@ func _ready():
     if get_multiplayer_authority() != multiplayer.get_unique_id():
         set_process(false)
         set_physics_process(false)
-    
-    # can i comment this out?
-    input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
 func _physics_process(_delta):
     input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-    
-
-# # Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta):
-# 	if Input.is_action_just_pressed("jump"):
-# 		jump.rpc()
-
-# @rpc("call_local")
-# func jump():
-# 	if multiplayer.is_server():
-# 		player.do_jump = true
