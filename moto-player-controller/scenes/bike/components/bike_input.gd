b17994f15670531e5aa28b1c4be5ed2ca90c0546
Sebastian Safari <ssebs@ymail.com>
2025-12-28 13:11:35 -0800

    lfs

--- moto-player-controller/scenes/bike/components/bike_input.gd
+++ moto-player-controller/scenes/bike/components/bike_input.gd
@@ -21,6 +21,9 @@ var gear_down_pressed: bool = false
 # Computed values
 var total_brake: float = 0.0
 
+# Vibration settings
+@export var vibration_duration: float = 0.15
+
 
 func _physics_process(_delta):
 	update_input()
@@ -39,3 +42,29 @@ func update_input():
 
 	gear_up_pressed = Input.is_action_just_pressed("gear_up")
 	gear_down_pressed = Input.is_action_just_pressed("gear_down")
+
+func update_vibration():
+	var weak_total = 0.0
+	var strong_total = 0.0
+	var _brake_vibe_intensity = 2.0
+
+	# Brake danger vibration
+	if state.brake_danger_level > 0.1:
+		weak_total += state.brake_danger_level * _brake_vibe_intensity
+		strong_total += state.brake_danger_level * state.brake_danger_level * _brake_vibe_intensity
+
+	# Fishtail vibration
+	var fishtail_intensity = abs(state.fishtail_angle) / state.max_fishtail_angle if state.max_fishtail_angle > 0 else 0.0
+	if fishtail_intensity > 0.1:
+		weak_total += fishtail_intensity * 0.6
+		strong_total += fishtail_intensity * fishtail_intensity * 0.8
+
+	# Apply vibration
+	if weak_total > 0.01 or strong_total > 0.01:
+		Input.start_joy_vibration(0, clamp(weak_total, 0.0, 1.0), clamp(strong_total, 0.0, 1.0), vibration_duration)
+	else:
+		stop_vibration()
+
+
+func stop_vibration():
+	Input.stop_joy_vibration(0)
