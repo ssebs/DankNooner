1a7856f768190c905fa0c2a0b8db97d5ac228be5
Sebastian Safari <ssebs@ymail.com>
2026-01-31 12:36:07 -0800

    play and lobby menu

--- v2/menus/out_of_game/play_menu/play_menu_state.gd
+++ v2/menus/out_of_game/play_menu/play_menu_state.gd
@@ -1,20 +1,28 @@
 @tool
-class_name LobbyMenuState extends MenuState
+class_name PlayMenuState extends MenuState
 
 @export var menu_manager: MenuManager
 @export var main_menu_state: MenuState
+@export var lobby_menu_state: MenuState
 
 @onready var back_btn: Button = %BackBtn
+@onready var lobby_btn: Button = %LobbyBtn
 
 
 func Enter():
 	ui.show()
 	back_btn.pressed.connect(_on_back_pressed)
+	lobby_btn.pressed.connect(_on_lobby_pressed)
 
 
 func Exit():
 	ui.hide()
 	back_btn.pressed.disconnect(_on_back_pressed)
+	lobby_btn.pressed.disconnect(_on_lobby_pressed)
+
+
+func _on_lobby_pressed():
+	transitioned.emit(lobby_menu_state)
 
 
 func _on_back_pressed():
