95614a7af0a9c9708f08b710b837597d69d14a1b
Sebastian Safari <ssebs@ymail.com>
2026-02-06 19:53:06 -0800

    start movement controller

--- v2/entities/player/player.gd
+++
@@ -1,2 +0,0 @@
-@tool
-class_name Player extends RigidBody3D
--- v2/levels/test_levels/test_01/test_01_level.gd
+++ v2/levels/test_levels/test_01/test_01_level.gd
@@ -1,3 +1,10 @@
 @tool
 extends LevelDefinition
 
+@export var player_entity_scene: PackedScene
+@export var player_spawn_pos: Marker3D
+@export var players_node: Node
+
+
+func _ready():
+	pass
