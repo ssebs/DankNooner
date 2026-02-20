8df94367091c91032cf8b53a7afa4d881307c769
Sebastian Safari <ssebs@ymail.com>
2026-02-19 21:49:44 -0800

    spawn mesh

--- v2/entities/player/characters/scripts/character_skin.gd
+++ v2/entities/player/characters/scripts/character_skin.gd
@@ -1,8 +1,25 @@
 @tool
 class_name CharacterSkin extends Node3D
 
-@export var mesh_res: Mesh = preload("res://entities/player/characters/assets/biker/biker_mesh.res")
-
+@export
+var mesh_res: PackedScene = preload("res://entities/player/characters/assets/clanker/Clanker.glb")
 @onready var anim_player: AnimationPlayer = %AnimationPlayer
-@onready var mesh_instance: MeshInstance3D = %MeshInstance3D
-@onready var skel: Skeleton3D = %Skeleton
+@onready var mesh_node: Node3D = %MeshNode
+
+# @onready var mesh_instance: MeshInstance3D = %MeshInstance3D
+# @onready var skel: Skeleton3D = %Skeleton
+
+
+func _ready():
+	spawn_mesh()
+
+
+func spawn_mesh():
+	for child in mesh_node.get_children():
+		child.queue_free()
+	var m = mesh_res.instantiate()
+	mesh_node.add_child(m)
+
+	# retarget AnimationMixer => Root Node to new mesh
+	anim_player.root_node = m.get_path()
+	anim_player.play("Biker/reset")
