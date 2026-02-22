c817aaaa6a3bae1e2462487676377fd601fd6dc8
Sebastian Safari <ssebs@ymail.com>
2026-02-22 14:27:36 -0800

    saving skins working, but dupe

--- v2/resources/entities/player/character_skin_definition.gd
+++ v2/resources/entities/player/character_skin_definition.gd
@@ -33,16 +33,17 @@ func save_to_disk():
 	var json_dict = {
 		"skin_name": skin_name,
 		"mesh_res": mesh_res.resource_path,
-		"primary_color": primary_color,  # will this work?
-		"secondary_color": secondary_color,  # will this work?
-		"back_marker_position": back_marker_position,  # will this work?
-		"back_marker_rotation_degrees": back_marker_rotation_degrees  # will this work?
+		"primary_color": DictJSONSaverLoader.color_to_dict(primary_color),
+		"secondary_color": DictJSONSaverLoader.color_to_dict(secondary_color),
+		"back_marker_position": DictJSONSaverLoader.vec3_to_dict(back_marker_position),
+		"back_marker_rotation_degrees":
+		DictJSONSaverLoader.vec3_to_dict(back_marker_rotation_degrees)
 	}
 	var path = SAVE_PATH.replace("ZZZ", skin_name.to_snake_case())
 	DictJSONSaverLoader.save_json_to_file(path, json_dict)
 
 
-## skin_name must be set first!
+## skin_name must be set before calling!
 func load_from_disk():
 	var path = SAVE_PATH.replace("ZZZ", skin_name.to_snake_case())
 
@@ -55,8 +56,11 @@ func load_from_disk():
 	mesh_res = load(
 		json_dict.get("mesh_res", "res://entities/player/characters/scenes/clanker_skin.tscn")
 	)
-	# TODO - convert these
-	primary_color = json_dict.get("primary_color", Color.TRANSPARENT)
-	secondary_color = json_dict.get("secondary_color", Color.TRANSPARENT)
-	back_marker_position = json_dict.get("back_marker_position", Vector3.ZERO)
-	back_marker_rotation_degrees = json_dict.get("back_marker_rotation_degrees", Vector3.ZERO)
+	primary_color = DictJSONSaverLoader.dict_to_color(json_dict.get("primary_color", {}))
+	secondary_color = DictJSONSaverLoader.dict_to_color(json_dict.get("secondary_color", {}))
+	back_marker_position = DictJSONSaverLoader.dict_to_vec3(
+		json_dict.get("back_marker_position", {})
+	)
+	back_marker_rotation_degrees = DictJSONSaverLoader.dict_to_vec3(
+		json_dict.get("back_marker_rotation_degrees", {})
+	)
