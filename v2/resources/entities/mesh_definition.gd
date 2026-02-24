0e4212872c0c31c80620d4af3238b1468cf29b44
Sebastian Safari <ssebs@ymail.com>
2026-02-23 20:30:54 -0800

    start bike skin definition

--- v2/resources/entities/mesh_definition.gd
+++
@@ -1,8 +0,0 @@
-@tool
-## All mesh objects should be defined from this
-class_name MeshDefinition extends Resource
-
-@export var mesh_scene: PackedScene
-@export var mesh_position_offset: Vector3 = Vector3.ZERO
-@export var mesh_rotation_offset_degrees: Vector3 = Vector3.ZERO
-@export var mesh_scale_multiplier: Vector3 = Vector3.ONE
