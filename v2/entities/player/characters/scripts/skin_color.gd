5e4277b0e1ccde1e125cbe7cf8ef2ceb7d0644aa
Sebastian Safari <ssebs@ymail.com>
2026-02-23 21:37:21 -0800

    add ability to use standard material in SkinColor

--- v2/entities/player/characters/scripts/skin_color.gd
+++ v2/entities/player/characters/scripts/skin_color.gd
@@ -2,39 +2,67 @@
 ## Meshes that you want to change the skin color should use this script
 class_name SkinColor extends Node3D
 
+@export_group("Primary")
 @export var primary_color: Color
-## should be a shader texture, see characters/shaders/skin_color.tres
+## Use StandardMaterial3D instead of shader material (for multi-material meshes)
+@export var primary_use_standard_material: bool = false
+## Which surface index to override. Meshes can have multiple materials; this targets a specific one.
+@export var primary_surface_index: int = 0
+## Used when primary_use_standard_material is true
+@export var primary_standard_material: StandardMaterial3D
+## Used when primary_use_standard_material is false (see characters/shaders/skin_color.tres)
 @export var primary_shader_material: ShaderMaterial
 @export var primary_mesh: MeshInstance3D
 
+@export_group("Secondary")
 @export var has_secondary: bool = false
 @export var secondary_color: Color
+@export var secondary_use_standard_material: bool = false
+## Which surface index to override. Meshes can have multiple materials; this targets a specific one.
+@export var secondary_surface_index: int = 0
+@export var secondary_standard_material: StandardMaterial3D
 @export var secondary_shader_material: ShaderMaterial
 @export var secondary_mesh: MeshInstance3D
 
-var primary_material: ShaderMaterial
-var secondary_material: ShaderMaterial
+var primary_material: Material
+var secondary_material: Material
 
 
 func _ready():
-	primary_material = primary_shader_material.duplicate()
-	primary_mesh.set_surface_override_material(0, primary_material)
+	if primary_use_standard_material:
+		primary_material = primary_standard_material.duplicate()
+	else:
+		primary_material = primary_shader_material.duplicate()
+	primary_mesh.set_surface_override_material(primary_surface_index, primary_material)
 	update_primary_color(primary_color)
 
 	if has_secondary:
-		secondary_material = secondary_shader_material.duplicate()
-		secondary_mesh.set_surface_override_material(0, secondary_material)
+		if secondary_use_standard_material:
+			secondary_material = secondary_standard_material.duplicate()
+		else:
+			secondary_material = secondary_shader_material.duplicate()
+		secondary_mesh.set_surface_override_material(secondary_surface_index, secondary_material)
 		update_secondary_color(secondary_color)
 
 
 func update_primary_color(color: Color):
 	primary_color = color
-	primary_material.set_shader_parameter("replacement_color", Vector3(color.r, color.g, color.b))
+	if primary_use_standard_material:
+		(primary_material as StandardMaterial3D).albedo_color = color
+	else:
+		primary_material.set_shader_parameter(
+			"replacement_color", Vector3(color.r, color.g, color.b)
+		)
 
 
 func update_secondary_color(color: Color):
 	secondary_color = color
-	secondary_material.set_shader_parameter("replacement_color", Vector3(color.r, color.g, color.b))
+	if secondary_use_standard_material:
+		(secondary_material as StandardMaterial3D).albedo_color = color
+	else:
+		secondary_material.set_shader_parameter(
+			"replacement_color", Vector3(color.r, color.g, color.b)
+		)
 
 
 func _get_configuration_warnings() -> PackedStringArray:
@@ -42,17 +70,21 @@ func _get_configuration_warnings() -> PackedStringArray:
 
 	if primary_mesh == null:
 		issues.append("primary_mesh must not be empty")
-	if primary_shader_material == null:
-		issues.append("primary_shader_material must not be empty")
-	if primary_color == null:
-		issues.append("primary_color must not be empty")
+	if primary_use_standard_material:
+		if primary_standard_material == null:
+			issues.append("primary_standard_material must not be empty")
+	else:
+		if primary_shader_material == null:
+			issues.append("primary_shader_material must not be empty")
 
 	if has_secondary:
 		if secondary_mesh == null:
 			issues.append("secondary_mesh must not be empty")
-		if secondary_shader_material == null:
-			issues.append("secondary_shader_material must not be empty")
-		if secondary_color == null:
-			issues.append("secondary_color must not be empty")
+		if secondary_use_standard_material:
+			if secondary_standard_material == null:
+				issues.append("secondary_standard_material must not be empty")
+		else:
+			if secondary_shader_material == null:
+				issues.append("secondary_shader_material must not be empty")
 
 	return issues
--- v2/resources/credits/assets.md
+++ v2/resources/credits/assets.md
@@ -6,3 +6,5 @@
 - Astronaut by Poly by Google [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/dLHpzNdygsg)
 - Mixamo
 - Animated Woman by Quaternius (https://poly.pizza/m/qJ2gsTUBHL)
+- https://www.cgtrader.com/free-3d-models/sports/game/low-poly-and-mid-poly-sport-motorcycle-3d-model-game-ready-asset
+- Naked bike "Motorcycle" (https://skfb.ly/oVrTZ) by eriklys is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
