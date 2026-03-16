33aecf33b5cd929d6d02d7c20e46901ffcbee745
Sebastian Safari <ssebs@ymail.com>
2026-03-15 17:57:34 -0700

    gearing controller

--- v2/entities/player/controllers/gearing_controller.gd
+++ v2/entities/player/controllers/gearing_controller.gd
@@ -11,13 +11,13 @@ signal rpm_updated(rpm_ratio: float)
 @export var clutch_release_speed: float = 2.5
 @export var clutch_tap_amount: float = 0.35
 
-var clutch_hold_time: float = 0.0
+var _clutch_hold_time: float = 0.0
 
-var current_gear: int = 1
-var current_rpm: float = 1000.0
-var clutch_value: float = 0.0
-var rpm_ratio: float = 0.0
-var is_stalled: bool = false
+var _current_gear: int = 1
+var _current_rpm: float = 1000.0
+var _clutch_value: float = 0.0
+var _rpm_ratio: float = 0.0
+var _is_stalled: bool = false
 
 ##
 # TODO - make sure to set player_entity's `nfx_gear_ratio`
@@ -28,59 +28,59 @@ func _ready():
 	if Engine.is_editor_hint():
 		return
 	input_controller.clutch_held_changed.connect(_on_clutch_input)
-	input_controller.gear_up_pressed.connect(_on_gear_shift.bind(1))
-	input_controller.gear_down_pressed.connect(_on_gear_shift.bind(-1))
+	input_controller.gear_change_pressed.connect(_on_gear_change)
 
 
-#region input handlers
+#region Local input handlers
 ## direction must be `1` or `-1` (fwd/back)
-func _on_gear_shift(direction: int):
-	# print("_on_gear_shift %d" % direction)
+func _on_gear_change(direction: int):
+	print("_on_gear_change %d" % direction)
 	var bd = player_entity.bike_definition
-	var new_gear = clampi(player_entity.current_gear + direction, 1, bd.num_gears)
-	if new_gear != player_entity.current_gear:
-		player_entity.current_gear = new_gear
+	var new_gear = clampi(_current_gear + direction, 1, bd.num_gears)
+	if new_gear != _current_gear:
+		_current_gear = new_gear
 		gear_changed.emit(new_gear)
 
 
 func _on_clutch_input(_held: bool, just_pressed: bool):
 	if just_pressed:
-		player_entity.clutch_value = minf(player_entity.clutch_value + clutch_tap_amount, 1.0)
+		_clutch_value = minf(_clutch_value + clutch_tap_amount, 1.0)
 
 
 #endregion
 
 
+func _physics_process(delta):
+	_update_clutch_hold_time(delta)
+
+
 ## Called from MovementController._rollback_tick()
 func on_movement_rollback_tick(delta: float):
-	_update_clutch_hold_time(delta)
-	_blend_rpm_to_target(delta)
+	_blend_rpm(delta)
 
-	player_entity.rpm_ratio = _get_rpm_ratio()
-	rpm_updated.emit(player_entity.rpm_ratio)
+	rpm_updated.emit(_get_rpm_ratio())
 
 
 func _update_clutch_hold_time(delta: float):
-	if input_controller.nfx_clutch_held:
-		clutch_hold_time += delta
-		player_entity.clutch_value = move_toward(
-			player_entity.clutch_value, 1.0, clutch_engage_speed * delta
-		)
+	if input_controller.clutch_held:
+		_clutch_hold_time += delta
+		_clutch_value = move_toward(_clutch_value, 1.0, clutch_engage_speed * delta)
 	else:
-		clutch_hold_time = 0.0
-		player_entity.clutch_value = move_toward(
-			player_entity.clutch_value, 0.0, clutch_release_speed * delta
-		)
+		_clutch_hold_time = 0.0
+		_clutch_value = move_toward(_clutch_value, 0.0, clutch_release_speed * delta)
 
 
-func _blend_rpm_to_target(delta: float):
+## Sets _current_rpm
+## We need to be in rollback tick
+func _blend_rpm(delta: float):
 	var bd = player_entity.bike_definition
-	var engagement = get_clutch_engagement()
+	var engagement = 1.0 - _clutch_value
+	var speed = player_entity.velocity.length()
 
 	# Calculate wheel-driven RPM
-	var gear_ratio = bd.gear_ratios[player_entity.current_gear - 1]
+	var gear_ratio = bd.gear_ratios[_current_gear - 1]
 	var gear_max_speed = bd.max_speed * (bd.gear_ratios[bd.num_gears - 1] / gear_ratio)
-	var speed_ratio = player_entity.speed / gear_max_speed if gear_max_speed > 0 else 0.0
+	var speed_ratio = speed / gear_max_speed if gear_max_speed > 0 else 0.0
 	var wheel_rpm = speed_ratio * bd.max_rpm
 
 	# Throttle-driven RPM
@@ -88,52 +88,50 @@ func _blend_rpm_to_target(delta: float):
 
 	# Blend based on clutch engagement
 	var target_rpm = lerpf(throttle_rpm, wheel_rpm, engagement)
-	player_entity.current_rpm = lerpf(player_entity.current_rpm, target_rpm, 8.0 * delta)
+	_current_rpm = lerpf(_current_rpm, target_rpm, 8.0 * delta)
 
 	# Limit RPM
-	player_entity.current_rpm = clamp(player_entity.current_rpm, bd.idle_rpm, bd.max_rpm)
+	_current_rpm = clamp(_current_rpm, bd.idle_rpm, bd.max_rpm)
 
 
+## Get pct of rpm : max rpm
 func _get_rpm_ratio() -> float:
 	var bd = player_entity.bike_definition
 	if bd.max_rpm <= bd.idle_rpm:
 		return 0.0
-	return (player_entity.current_rpm - bd.idle_rpm) / (bd.max_rpm - bd.idle_rpm)
+	return (_current_rpm - bd.idle_rpm) / (bd.max_rpm - bd.idle_rpm)
 
 
 #region public api
 ## Returns power multiplier (0-1) based on current RPM and gear
 func get_power_output() -> float:
-	if is_stalled:
+	if _is_stalled:
 		return 0.0
 
-	var engagement = get_clutch_engagement()
-	if engagement < 0.05:
+	var engagement = 1.0 - _clutch_value
+	if _clutch_value > 0.05:
 		return 0.0
 
 	var ratio = _get_rpm_ratio()
+	# TODO - use actual curve
 	var power_curve = ratio * (2.0 - ratio)  # Peaks around 75% RPM
 
 	var bd = player_entity.bike_definition
-	var gear_ratio = bd.gear_ratios[player_entity.current_gear - 1]
+	var gear_ratio = bd.gear_ratios[_current_gear - 1]
 	var base_ratio = bd.gear_ratios[bd.num_gears - 1]
 	var torque_multiplier = gear_ratio / base_ratio
 
 	return input_controller.nfx_throttle * power_curve * torque_multiplier * engagement
 
 
-func get_clutch_engagement() -> float:
-	return 1.0 - player_entity.clutch_value
-
-
 ## Called from player_entity.gd's do_respawn
 func do_reset():
-	current_gear = 1
-	current_rpm = (
+	_current_gear = 1
+	_current_rpm = (
 		player_entity.bike_definition.idle_rpm if player_entity.bike_definition else 1000.0
 	)
-	clutch_value = 0.0
-	rpm_ratio = 0.0
+	_clutch_value = 0.0
+	_rpm_ratio = 0.0
 
 
 #endregion
