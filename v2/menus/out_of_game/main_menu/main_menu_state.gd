454825453c2794b6d34eada0014c6cb2e7f9ac78
Sebastian Safari <ssebs@ymail.com>
2026-04-06 22:11:32 -0700

    on main menu

--- v2/menus/out_of_game/main_menu/main_menu_state.gd
+++ v2/menus/out_of_game/main_menu/main_menu_state.gd
@@ -6,11 +6,13 @@ class_name MainMenuState extends MenuState
 @export var settings_menu_state: MenuState
 @export var play_menu_state: MenuState
 @export var customize_menu_state: MenuState
+@export var help_menu_state: MenuState
 
 @onready var settings_btn: Button = %SettingsBtn
 @onready var play_btn: Button = %PlayBtn
 @onready var customize_btn: Button = %CustomizeBtn
 @onready var quit_btn: Button = %QuitBtn
+@onready var help_btn: Button = %HelpBtn
 @onready var version_label: Label = %VersionLabel
 
 
@@ -30,6 +32,7 @@ func Enter(_state_context: StateContext):
 	settings_btn.pressed.connect(_on_settings_btn_pressed)
 	quit_btn.pressed.connect(_on_quit_btn_pressed)
 	customize_btn.pressed.connect(_on_customize_btn_pressed)
+	help_btn.pressed.connect(_on_help_btn_pressed)
 
 	if level_manager.current_level_name != LevelManager.LevelName.BG_GRAY_LEVEL:
 		level_manager.spawn_menu_level()
@@ -42,9 +45,14 @@ func Exit(_state_context: StateContext):
 	settings_btn.pressed.disconnect(_on_settings_btn_pressed)
 	quit_btn.pressed.disconnect(_on_quit_btn_pressed)
 	customize_btn.pressed.disconnect(_on_customize_btn_pressed)
+	help_btn.pressed.disconnect(_on_help_btn_pressed)
 
 
 #region button handlers
+func _on_help_btn_pressed():
+	transitioned.emit(help_menu_state, StateContext.NewWithReturn(self))
+
+
 func _on_customize_btn_pressed():
 	transitioned.emit(customize_menu_state, StateContext.NewWithReturn(self))
 
