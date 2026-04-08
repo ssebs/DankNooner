8de2c94ff7681b53f906a7c9354bc024c5e7959c
Sebastian Safari <ssebs@ymail.com>
2026-04-07 21:01:10 -0700

    should be working but no

--- v2/managers/gamemodes/free_roam/free_roam_gamemode.gd
+++ v2/managers/gamemodes/free_roam/free_roam_gamemode.gd
@@ -1,1 +1,43 @@
+@tool
 class_name FreeRoamGameMode extends GameMode
+
+var _respawn_delay: float = 3.0
+
+
+func Enter(_state_context: StateContext):
+	if Engine.is_editor_hint():
+		return
+
+	gamemode_manager.current_game_mode = GamemodeManager.TGameMode.FREE_FROAM
+	print("FreeRoam")
+
+	gamemode_manager.player_crashed.connect(_on_player_crashed)
+	gamemode_manager.player_disconnected.connect(_on_player_disconnected)
+	gamemode_manager.player_latejoined.connect(_on_player_latejoined)
+
+	spawn_manager.spawn_all_players()
+
+
+func Exit(_state_context: StateContext):
+	if Engine.is_editor_hint():
+		return
+	gamemode_manager.player_crashed.disconnect(_on_player_crashed)
+	gamemode_manager.player_disconnected.disconnect(_on_player_disconnected)
+	gamemode_manager.player_latejoined.disconnect(_on_player_latejoined)
+
+
+func _on_player_crashed(peer_id: int):
+	if !multiplayer.is_server():
+		return
+	get_tree().create_timer(_respawn_delay).timeout.connect(
+		func(): spawn_manager.respawn_player.rpc(peer_id), CONNECT_ONE_SHOT
+	)
+
+
+func _on_player_latejoined(peer_id: int):
+	gamemode_manager.latespawn_player(peer_id)
+
+
+func _on_player_disconnected(peer_id: int):
+	if gamemode_manager.match_state == GamemodeManager.MatchState.IN_GAME:
+		spawn_manager.rpc_despawn_player.rpc(peer_id)
--- v2/managers/gamemodes/tutorial/tutorial_gamemode.gd
+++ v2/managers/gamemodes/tutorial/tutorial_gamemode.gd
@@ -1,1 +1,2 @@
+@tool
 class_name TutorialGameMode extends GameMode
