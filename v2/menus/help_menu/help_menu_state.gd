44c7b8762218cfc492dcda91b6c7ecf19787b83e
Sebastian Safari <ssebs@ymail.com>
2026-04-07 17:55:05 -0700

    change controls to a tab view

--- v2/menus/help_menu/help_menu_state.gd
+++ v2/menus/help_menu/help_menu_state.gd
@@ -3,12 +3,8 @@ class_name HelpMenuState extends MenuState
 
 @export var menu_manager: MenuManager
 
-@onready var gamepad_btn: Button = %GamepadBtn
-@onready var kbm_btn: Button = %KBMBtn
+@onready var tab_container: TabContainer = %TabContainer
 @onready var close_help_btn: Button = %CloseHelpBtn
-
-@onready var gamepad_img: TextureRect = %GamepadImg
-@onready var kbm_img: TextureRect = %KBMImg
 @onready var bg_tint: ColorRect = %BGTint
 
 
@@ -24,33 +20,19 @@ func Enter(state_context: StateContext):
 	ui.show()
 	_show_controls_for_current_device()
 
-	gamepad_btn.pressed.connect(_on_gamepad_pressed)
-	kbm_btn.pressed.connect(_on_kbm_pressed)
 	close_help_btn.pressed.connect(_on_close_help_pressed)
 
 
 func Exit(_state_context: StateContext):
 	ui.hide()
-	gamepad_btn.pressed.disconnect(_on_gamepad_pressed)
-	kbm_btn.pressed.disconnect(_on_kbm_pressed)
 	close_help_btn.pressed.disconnect(_on_close_help_pressed)
 
 
 func _show_controls_for_current_device():
 	if Input.get_connected_joypads().size() > 0:
-		_on_gamepad_pressed()
+		tab_container.current_tab = 0
 	else:
-		_on_kbm_pressed()
-
-
-func _on_gamepad_pressed():
-	gamepad_img.show()
-	kbm_img.hide()
-
-
-func _on_kbm_pressed():
-	kbm_img.show()
-	gamepad_img.hide()
+		tab_container.current_tab = 1
 
 
 func _on_close_help_pressed():
