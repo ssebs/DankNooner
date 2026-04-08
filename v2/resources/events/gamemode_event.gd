14adab279f8b6c3f117a78168c4dd086c6229d16
Sebastian Safari <ssebs@ymail.com>
2026-04-07 22:24:26 -0700

    create gamemode hud

--- v2/resources/events/gamemode_event.gd
+++ v2/resources/events/gamemode_event.gd
@@ -3,3 +3,5 @@ class_name GameModeEvent extends Resource
 
 ## Will use localization in rendering
 @export var name: String
+@export
+var description: String = "Sunt nisi id proident veniam ad laboris pariatur minim eu commodo aliquip."
