b93278cbe49a0fcf5a983e641f016ecab99dfed2
Sebastian Safari <sebsafari@gmail.com>
2026-09-08 22:18:10 -0700

    start to build 2nd stunt race

--- v2/managers/gamemodes/gamemode_state_context.gd
+++ v2/managers/gamemodes/gamemode_state_context.gd
@@ -3,3 +3,6 @@ class_name GamemodeStateContext extends StateContext
 var gamemode_event: GameModeEventDefinition
 var event_start_circle: EventStartCircle
 var peer_id: int = -1
+## Set when returning to free roam after finishing a race — tells FreeRoamGameMode to leave
+## players where they finished instead of redistributing them to the grid.
+var skip_spawn_redistribute: bool = false
