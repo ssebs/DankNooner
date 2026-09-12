d6b1089b47006426de581916beface91efbb1b38
Sebastian Safari <sebsafari@gmail.com>
2026-09-12 13:45:06 -0700

    new checkpoint markers

--- v2/managers/gamemodes/gamemodeobjects/checkpoint_marker.gd
+++ v2/managers/gamemodes/gamemodeobjects/checkpoint_marker.gd
@@ -3,39 +3,25 @@
 ## Reused by tutorial checkpoint objectives and by Street Race checkpoints.
 class_name CheckPointMarker extends GameModeObject
 
-@export var width: float = 8.0:
+@export var width: float = 16.0:
 	set(value):
 		width = value
 		_apply_width()
-@export var sign_text: String = "REPLACE_ME":
-	set(value):
-		sign_text = value
-		_apply_sign_text()
 
 @onready var _trigger_shape: CollisionShape3D = %CollisionShape3D
-@onready var _label: Label3D = %Label3D
-@onready var _sign: GrayBoxStaticBody = %SignNoCollision
-@onready var _pillar_left: Node3D = %LeftPillar
-@onready var _pillar_right: Node3D = %RightPillar
+@onready var _border_left: Node3D = %BorderCylLeft
+@onready var _border_right: Node3D = %BorderCylRight
 
 
 func _ready():
 	super()
 	_trigger_shape.shape = BoxShape3D.new()
 	_apply_width()
-	_apply_sign_text()
 
 
 func _apply_width():
 	if !is_node_ready():
 		return
-	(_trigger_shape.shape as BoxShape3D).size = Vector3(width, 4, 1)
-	_sign.width = width
-	_pillar_left.position.x = -width / 2.0
-	_pillar_right.position.x = width / 2.0
-
-
-func _apply_sign_text():
-	if !is_node_ready():
-		return
-	_label.text = sign_text
+	(_trigger_shape.shape as BoxShape3D).size = Vector3(width, 64, 1)
+	_border_left.position.x = - width / 2.0
+	_border_right.position.x = width / 2.0
--- v2/planning_docs/_ScratchPad.md
+++ v2/planning_docs/_ScratchPad.md
@@ -6,10 +6,11 @@
 - [x] increase bat time
 - [x] make grass unstable
 - [x] reverse + turn on unstable collision should not make you crash
-- [ ] add more arrow signs in world
-- [ ] mid race respawn multiple people shouldnt spawn in same place
+- [x] burnout sound doesnt loop
 - [ ] move checkpoints so jumping over it counts
   - [ ] like forza horizon, cant crash into gates, no height limit
+- [ ] add more arrow signs in world
+- [ ] mid race respawn multiple people shouldnt spawn in same place
 - [ ] peeps can start race before countdown
 - [ ] lightning / Rally Up item
 - [ ] r => quick respawn, add btn for set full respawn point (if not in race)
@@ -24,4 +25,3 @@
   - do X trick in 10s or explode, see - CS2 TDM
 - auto transmission => reverse shouldnt need clutch
 - TP commands dont work on multiplayer
-- burnout sound doesnt loop
